08de1a260b7fa5c552773ebcc0131d5befc259ce
Muharem Kapo <mkapo3@etf.unsa.ba>
2022-12-11 07:13:43 +0100

    Fixed barrier sync

--- graph.cpp
+++ graph.cpp
@@ -1,28 +1,66 @@
 #include <ctime>
 #include "graph.cuh"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void convert_to_numeric(std::string value, int& a, int& b) {
+    std::string delimiter = "\t";
+
+    size_t pos = 0;
+    std::string token;
+    pos = value.find(delimiter);
+    token = value.substr(0, pos);
+    value.erase(0, pos + delimiter.length());
+    a = stoi(token);
+    b = stoi(value);
+}
 
 void readGraph(Graph& G) {
     int n;
     int m;
+    bool console_input = false;
+
+    
+    std::string n_vertices_n_edges;
+
+    
+    std::ifstream MyReadFile("p2p-Gnutella08.txt");
+    
+    
+
+   
 
     //If no arguments then read graph from stdin
     //bool fromStdin = argc <= 2;
-    if (true) {
+    
+
+    if (console_input) {
         scanf("%d %d", &n, &m);
     }
+    else {
+        std::getline(MyReadFile, n_vertices_n_edges);
+        convert_to_numeric(n_vertices_n_edges, n, m);
+    }
+   
     /*else {
         srand(12345);
         n = atoi(argv[2]);
         m = atoi(argv[3]);
     }*/
-
+    std::string edge;
     std::vector<std::vector<int> > adjecancyLists(n);
+    
     for (int i = 0; i < m; i++) {
         int u, v;
-        if (true) {
+        if (console_input) {
             scanf("%d %d", &u, &v);
-            adjecancyLists[u].push_back(v);
         }
+        else {
+            getline(MyReadFile, edge);
+            convert_to_numeric(edge, u, v);
+        }
+        adjecancyLists[u].push_back(v);
         /*else {
             u = rand() % n;
             v = rand() % n;
@@ -30,6 +68,8 @@ void readGraph(Graph& G) {
             adjecancyLists[v].push_back(u);
         }*/
     }
+
+    MyReadFile.close();
 
     for (int i = 0; i < n; i++) {
         G.edgesOffset.push_back(G.adjacencyList.size());
--- test_1.cu
+++ test_1.cu
@@ -3,9 +3,11 @@
 #include "device_launch_parameters.h"
 #include "graph.cuh"
 #include <stdio.h>
+#include <chrono>
 
 __global__ void kernel_cuda_simple(
 	int num_vertices,
+	int level,
 	int* v_adj_length,
 	int* v_adj_list,
 	int* v_adj_begin,
@@ -15,15 +17,15 @@ __global__ void kernel_cuda_simple(
  {
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	int num_threads = blockDim.x * gridDim.x;
-	
+	/*printf("Trenutni level je: %d\n", level);*/
 
 	for (int v = 0; v < num_vertices; v += num_threads)
 	{
 		int vertex = v + tid;
 		//printf("%d ", num_vertices);
-		if (vertex < num_vertices)
+		if (result[vertex] == level && vertex < num_vertices)
 		{
-			
+			//printf("Usao u vertex broj: %d\n", vertex);
 			
 			//printf("Velicina adjacency matrice za vertex %d: %d\n", vertex, v_adj_length[vertex]);
 			for (int n = 0; n < v_adj_length[vertex]; n++)
@@ -33,16 +35,16 @@ __global__ void kernel_cuda_simple(
 				//printf("Susjed cvora %d je: %d\n",vertex, neighbor);
 					if (result[neighbor] > result[vertex] + 1)
 					{
-						printf("%d %d\n", result[neighbor], result[vertex] + 1);
+						//printf("%d %d\n", result[neighbor], result[vertex] + 1);
 						result[neighbor] = result[vertex] + 1;
 						* still_running = 1;
-						printf("Rezultat za izvorni cvor %d je %d, a za ciljni cvor %d je %d\n", vertex, result[vertex], neighbor, result[neighbor]);
+						//printf("Rezultat za izvorni cvor %d je %d, a za ciljni cvor %d je %d\n", vertex, result[vertex], neighbor, result[neighbor]);
 					}
 				
 			}
 		}
 	}
-	printf("\n");
+	//printf("\n");
 }
 
  void run(Graph G, std::vector<int> &resultVector)
@@ -50,8 +52,9 @@ __global__ void kernel_cuda_simple(
 	 
 	 int N = G.numVertices;
 	 
-	 int * d_adj_length, * d_adj_list, * d_adj_begin, *d_result;
+	 int * d_adj_length, * d_adj_list, * d_adj_begin, *d_result, d_level;
 	 bool* d_still_running;
+	 
 	 //bool* h_test = new bool(true);
 	 
 
@@ -66,7 +69,7 @@ __global__ void kernel_cuda_simple(
 
 
 	 int BLOCKS = 1;
-	 dim3 THREADS(N, 1, 1);
+	 dim3 THREADS(128, 1, 1);
 
 	 int* adj_length = &(*G.edgesSize.begin());
 	 int* adj_list = &(*G.adjacencyList.begin());
@@ -78,15 +81,19 @@ __global__ void kernel_cuda_simple(
 	 cudaMalloc(&d_adj_list, G.adjacencyList.size() * sizeof(int));
 	 cudaMalloc(&d_adj_begin, G.edgesOffset.size() * sizeof(int));
 	 cudaMalloc(&d_result, N * sizeof(int));
-
 	 
 
-	 //printf("%d ", adj_begin[0]);
+	 printf("Velicina edges size: %d\n", G.edgesSize.size());
+	 printf("Velicina adjacency list: %d\n", G.adjacencyList.size());
+	 printf("Velicina edges offset: %d\n", G.edgesOffset.size());
+
+	 printf("Starting simple parallel bfs.\n");
+	 auto start = std::chrono::steady_clock::now();
+	 int level = 0;
 
 	 while (*still_running) {
 		 {
 			 *still_running = false;
-			 *k_still_running = false;
 			 cudaMemcpy(d_still_running, still_running, sizeof(bool) * 1, cudaMemcpyHostToDevice);
 			 cudaMemcpy(d_adj_length, adj_length, G.edgesSize.size() * sizeof(int), cudaMemcpyHostToDevice);
 			 cudaMemcpy(d_adj_list, adj_list, G.adjacencyList.size() * sizeof(int), cudaMemcpyHostToDevice);
@@ -94,25 +101,34 @@ __global__ void kernel_cuda_simple(
 			 cudaMemcpy(d_result, result, N * sizeof(int), cudaMemcpyHostToDevice);
 			 //cudaMemcpy(d_y, y, N * sizeof(int), cudaMemcpyHostToDevice);
 			 
-			 for (int i = 0; i < N; i++) {
+			 /*for (int i = 0; i < N; i++) {
 				 printf("%d ", result[i]);
 			 }
-			 printf("\n");
+			 printf("\n");*/
 
-			 kernel_cuda_simple <<<BLOCKS, THREADS>>> (N, d_adj_length, d_adj_list, d_adj_begin, d_result, d_still_running);
+			 kernel_cuda_simple <<<BLOCKS, THREADS>>> (N, level, d_adj_length, d_adj_list, d_adj_begin, d_result, d_still_running);
 			 //cudaLaunchKernel(kernel_cuda_simple, G.numVertices / 1024 + 1, 1024, 1, args, 0, 0);
 			 cudaMemcpy(still_running, d_still_running, sizeof(bool) * 1, cudaMemcpyDeviceToHost);
 			 cudaMemcpy(adj_length, d_adj_length, G.edgesSize.size() * sizeof(int), cudaMemcpyDeviceToHost);
 			 cudaMemcpy(adj_list, d_adj_list, G.adjacencyList.size() * sizeof(int), cudaMemcpyDeviceToHost);
 			 cudaMemcpy(adj_begin, d_adj_begin, G.edgesOffset.size() * sizeof(int), cudaMemcpyDeviceToHost);
 			 cudaMemcpy(result, d_result, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+			 level++;
+			 
 		 }
 	 }
 	
-	cudaThreadSynchronize();
+	cudaDeviceSynchronize();
 	cudaFree(still_running);
 	cudaFree(k_still_running);
-
+	cudaFree(d_adj_length);
+	cudaFree(d_adj_list);
+	cudaFree(d_adj_begin);
+	cudaFree(d_result);
+	auto end = std::chrono::steady_clock::now();
+	long duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	printf("Elapsed time in milliseconds : %li ms.\n", duration);
  }
 
  int main() {
@@ -136,7 +152,7 @@ __global__ void kernel_cuda_simple(
 
 	 run(G, resultVector);
 	 for (int i = 0; i < resultVector.size(); i++) {
-		 printf("%d  ", resultVector[i]);
+		 printf("%d %d  \n",i, resultVector[i]);
 	 }
 	 return 0;
  }
